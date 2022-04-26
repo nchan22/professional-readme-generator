5ae2af0263a3ee259d64567b361d124b403647cb
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-04-25 20:22:24 -0400

    Added writeFile function

--- Develop/index.js
+++ Develop/index.js
@@ -117,8 +117,12 @@ const questions = [
     }
 ];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        err ? console.log(err) : console.log("Created readme successfully");
+    });
+}
 
 // TODO: Create a function to initialize app
 function init() {}
