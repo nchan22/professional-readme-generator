5ca5fc5e2e09e12b65bef780a476f539ea86473f
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-04-25 20:24:17 -0400

    added required const

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // Array of questions for user input for README file
 const questions = [
